f923d4d42f7fa0f77ab2e2a9afb2ec851ad35502
agnetjoseph@gmail.com <agnetjoseph@gmail.com>
2026-07-15 04:15:22 +0530

    navbar completed

--- src/components/ViewTeam.jsx
+++ src/components/ViewTeam.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import NavBar from './NavBar'
 
 const ViewTeam = () => {
 
@@ -21,7 +22,7 @@ const ViewTeam = () => {
 
     return (
         <div className="container">
-             
+             <NavBar />
 
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
